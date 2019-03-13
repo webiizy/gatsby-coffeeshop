6380596a3f0715ab90b70c679380bbe9d6582bc7
Ha Minh <minh.ha@spt.vn>
2019-03-13 12:22:37 +0700

    add about page

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react"
+import Layout from "../components/layout"
+
+export default class about extends Component {
+  render() {
+    return (
+      <Layout>
+        <div>About page</div>
+      </Layout>
+    )
+  }
+}
