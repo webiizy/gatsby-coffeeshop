da1eb29d675ed62508e3ae810a592ac34f5774de
Minh <webiizy@gmail.com>
2019-03-14 22:47:52 +0700

    about

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,7 +1,33 @@
-import React, { Component } from "react";
+import React from "react";
+import { Link, graphql } from "gatsby";
 
-export default class about extends Component {
-  render() {
-    return <div>About</div>;
+import Layout from "../components/layout";
+import SEO from "../components/seo";
+import BackgroundSection from "../components/Globals/BackgroundSection";
+import Info from "../components/Globals/Info";
+
+const AboutPage = ({ data }) => (
+  <Layout>
+    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
+    <BackgroundSection
+      img={data.img.childImageSharp.fluid}
+      title="About Our"
+      styleClass="about-background"
+    />
+    <Info />
+  </Layout>
+);
+
+export const query = graphql`
+  {
+    img: file(relativePath: { eq: "about-background.jpeg" }) {
+      childImageSharp {
+        fluid {
+          ...GatsbyImageSharpFluid_tracedSVG
+        }
+      }
+    }
   }
-}
+`;
+
+export default AboutPage;
